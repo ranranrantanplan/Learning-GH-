1afbfc572eec16df9ee43bcedca1539dbc3b62b8
Daniel Aeschlimann <daniel.aeschlimann@me.com>
2022-01-21 10:42:26 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Daniel Aeschlimann on 21.01.22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
